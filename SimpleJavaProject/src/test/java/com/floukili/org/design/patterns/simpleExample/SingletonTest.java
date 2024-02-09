3ee5b75529c4b68f0aeb71a0ab8410bdc7627216
unknown <fatimazahraloukili02@gmail.com>
2024-02-09 16:38:36 +0100

    @test

--- SimpleJavaProject/src/test/java/com/floukili/org/design/patterns/simpleExample/SingletonTest.java
+++ SimpleJavaProject/src/test/java/com/floukili/org/design/patterns/simpleExample/SingletonTest.java
@@ -1,18 +1,15 @@
 package com.floukili.org.design.patterns.simple.example;
 
-import com.floukili.org.design.patterns.simple.example.Singleton;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import org.junit.jupiter.api.Test;
- 
 
-public class SingletonTest{
-	
-	public void testSingletonInstance() {
-		
-		 Singleton instance1 = Singleton.getInstance();
-	     Singleton instance2 = Singleton.getInstance();
-	     
-	     assertSame(instance1, instance2);
-	}
+public class SingletonTest {
+
+    @Test
+    public void testSingletonInstance() {
+        Singleton instance1 = Singleton.getInstance();
+        Singleton instance2 = Singleton.getInstance();
+
+        assertSame(instance1, instance2);
+    }
 }
